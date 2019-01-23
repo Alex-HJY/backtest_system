f6a345109f9a0c1b8702d2307a77fb2edae60f83
alex <156979387@qq.com>
2019-01-23 16:49:05 +0800

    初步框架，需要补充SQL链接

--- backtest_system.py
+++ backtest_system.py
@@ -6,7 +6,6 @@ By Alex_HJY
 """
 
 from copy import deepcopy
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,7 +15,8 @@ from datetime import datetime
 
 
 class backtest_system:
-    def __init__(self, price_data=pd.DataFrame(), benchmark_code='', initial_money=1000000, save_dir='', start_date='', end_date=''):
+    def __init__(self, price_data=pd.DataFrame(), benchmark_code='', initial_money=1000000, save_dir='', start_date='',
+                 end_date=''):
         """
         构建回测系统时的初始化函数
 
@@ -27,13 +27,13 @@ class backtest_system:
         :return backtest_result: 回测结果,DataFrame格式
         """
         self.benchmark_code = benchmark_code
-        self.start_date=parser.parse(start_date)
-        self.end_date= parser.parse(end_date)
+        self.start_date = parser.parse(start_date)
+        self.end_date = parser.parse(end_date)
         self.strategies_data = {}  # 存放本系统中运行的策略对应的回测数据
         self.strategies_indexes = {}  # 存放本系统中运行的策略对应的回测指标
         self.price_data = deepcopy(price_data)
-        self.price_data.index=pd.DatetimeIndex(self.price_data.index)
-        self.benchmark_value=self.cal_benchmark_value(benchmark_code)
+        self.price_data.index = pd.DatetimeIndex(self.price_data.index)
+        self.benchmark_value = self.cal_benchmark_value(benchmark_code)
         self.init_money = initial_money
         self.save_dir = save_dir
         self.backtest_result = pd.DataFrame()
@@ -46,13 +46,12 @@ class backtest_system:
         pass
 
     def cal_benchmark_value(self, benchmark_code):
-        benchmark_price=self.price_data[benchmark_code]
-        benchmark_price=benchmark_price[self.start_date:self.end_date]
-        share=benchmark_price.iloc[0]/self.init_money
-        return benchmark_price*share
+        benchmark_price = self.price_data[benchmark_code]
+        benchmark_price = benchmark_price[self.start_date:self.end_date]
+        share = benchmark_price.iloc[0] / self.init_money
+        return benchmark_price * share
 
     # =====================需要重写
-
 
     def back_test_by_day(self, strategy_name='', trade_func='', before_trade_func='',
                          after_trade_func='', show=True):
@@ -81,7 +80,7 @@ class backtest_system:
         today = start_date
         bench_mark = self.benchmark_code
 
-        backtest_result.index=price_data.index
+        backtest_result.index = price_data.index
         backtest_result['value'] = ''
         backtest_result['cash'] = ''
         backtest_result['portfolio'] = ''
@@ -97,8 +96,7 @@ class backtest_system:
             today = today + one_day
 
         self.strategies_data[strategy_name] = backtest_result
-        self.strategies_indexes[strategy_name]=self.calculate_indexes([strategy_name])
-
+        self.strategies_indexes[strategy_name] = self.calculate_indexes([strategy_name])
 
         if show:
             self.show([strategy_name])
@@ -108,9 +106,6 @@ class backtest_system:
 
         return backtest_result
 
-
-
-
     def show(self, strategies_name=[]):
         """
 
@@ -119,70 +114,130 @@ class backtest_system:
         """
         plt.plot(self.benchmark_value.index, self.benchmark_value, label=self.benchmark_code)
         for strategy in strategies_name:
-            df=self.strategies_data[strategy]
+            df = self.strategies_data[strategy]
             plt.plot(df.index, df['money'], label=strategy)
         plt.legend()
         plt.show()
         return None
 
-
-
-#================需要重写
-    def calculate_indexes(self,strategies_name=[]):
+    # ================需要重写，代码冗余
+    def calculate_indexes(self, strategies_name=[]):
         """
 
         :param strategies_name: 策略名称，数组
         :return: 无
         """
-        pass
 
-    # def get_indexes(self, df, strategies_name=[]):
-    #     indexes = pd.DataFrame()
-    #     for name in strategies_name:
-    #         # 年化收益率
-    #         r = (df.iloc[-1][name] * 1.0 / df.iloc[0][name]) ** (250 * 1.0 / df[name].__len__()) - 1
-    #
-    #         # 回撤
-    #         d = 0
-    #         t = df.iloc[0][name]
-    #         for i in range(1, df[name].__len__()):
-    #             t = max(t, df.iloc[i][name] * 1.0)
-    #             d = max(d, 1 - df.iloc[i][name] * 1.0 / t)
-    #         # sharp
-    #         rt = []
-    #         for i in range(df.index[0].year, df.index[-1].year):
-    #             rt.append((df[str(i)].ix[-1, name] - df[str(i)].ix[0, name]) * 1.0 / df[str(i)].ix[0, name] - 1)
-    #         rt = np.array(rt)
-    #         rt = rt * 100
-    #         sigma = rt.std()
-    #         sharp = (r - 0.028) * 100 / sigma
-    #
-    #         # 胜率
-    #         win = df[(df[name] - df[name].shift(-1)) > 0][name].count() * 1.0 / df.__len__()
-    #
-    #         # beta alpha
-    #         df[name + '_rtn'] = (df[name] - df[name].shift(-1)) / df[name] * 100
-    #
-    #         df[self.bench_mark + '_rtn'] = (df[self.bench_mark + '_money'] - df[self.bench_mark + '_money'].shift(-1)) / \
-    #                                        df[self.bench_mark + '_money'] * 100
-    #         df[name + '_rtn'] = df[name + '_rtn'].map(float)
-    #         df[self.bench_mark + '_rtn'] = df[self.bench_mark + '_rtn'].map(float)
-    #         beta = df[name + '_rtn'][:-1].cov(df[self.bench_mark + '_rtn'][:-1]) / df[self.bench_mark + '_rtn'][
-    #                                                                                :-1].var()
-    #         alpha = (r - 0.0284) - beta * (
-    #                 ((df.iloc[-1][self.bench_mark + '_money'] * 1.0 / df.iloc[0][self.bench_mark + '_money']) ** (
-    #                         250 * 1.0 / df.__len__()) - 1) - 0.0284)
-    #
-    #         index = {'策略收益': [str(((df.iloc[-1][name] * 1.0 / df.iloc[0][name])) * 100)],
-    #                  '基准收益': [str(((df.iloc[-1][self.bench_mark + '_money'] * 1.0 / df.iloc[0][
-    #                      self.bench_mark + '_money'])) * 100)],
-    #
-    #                  '策略收益率': [str(r * 100) + '%'],
-    #                  '回撤': [str(d * 100) + '%'],
-    #                  'sharp': [str(sharp)],
-    #                  '上涨率': [str(win)],
-    #                  'alpha': [alpha],
-    #                  'beta': [beta]}
-    #         index = pd.DataFrame(index)
-    #     indexes = indexes.append(index)
-    #     return indexes
+        indexes = pd.DataFrame()
+        for name in strategies_name:
+            df = deepcopy(self.strategies_data[name])
+            df[self.benchmark_code + '_money'] = self.benchmark_value
+            # 年化收益率
+            r = (df.ix[-1, 'value'] * 1.0 / df.ix[0, 'value']) ** (250 * 1.0 / df['value'].__len__()) - 1
+
+            # 回撤
+            d = 0
+            t = df.ix[0, 'value']
+            for i in range(1, df['value'].__len__()):
+                t = max(t, df.ix[i, 'value'] * 1.0)
+                d = max(d, 1 - df.ix[i, 'value'] * 1.0 / t)
+            # sharp
+            rt = []
+            for i in range(df.index[0].year, df.index[-1].year):
+                rt.append(
+                    (df[str(i)].ix[-1, 'value'] - df[str(i)].ix[0, 'value']) * 1.0 / df[str(i)].ix[0, 'value'] - 1)
+            rt = np.array(rt)
+            rt = rt * 100
+            sigma = rt.std()
+            sharp = (r - 0.028) * 100 / sigma
+
+            # 胜率
+            win = df[(df['value'] - df['value'].shift(-1)) > 0]['value'].count() * 1.0 / df.__len__()
+
+            # beta alpha
+            df[name + '_rtn'] = (df[name] - df['value'].shift(-1)) / df['value'] * 100
+
+            df[self.benchmark_code + '_rtn'] = (df[self.benchmark_code + '_money'] - df[
+                self.benchmark_code + '_money'].shift(-1)) / \
+                                               df[self.benchmark_code + '_money'] * 100
+            df[name + '_rtn'] = df[name + '_rtn'].map(float)
+            df[self.benchmark_code + '_rtn'] = df[self.benchmark_code + '_rtn'].map(float)
+            beta = df[name + '_rtn'][:-1].cov(df[self.benchmark_code + '_rtn'][:-1]) / df[self.benchmark_code + '_rtn'][
+                                                                                       :-1].var()
+            alpha = (r - 0.0284) - beta * (
+                    ((df.iloc[-1][self.benchmark_code + '_money'] * 1.0 / df.iloc[0][
+                        self.benchmark_code + '_money']) ** (
+                             250 * 1.0 / df.__len__()) - 1) - 0.0284)
+
+            index = {'策略收益': [str((df.iloc[-1]['value'] * 1.0 / df.iloc[0]['value']) * 100)],
+                     '基准收益': [str(((df.iloc[-1][self.benchmark_code + '_money'] * 1.0 / df.iloc[0][
+                         self.benchmark_code + '_money'])) * 100)],
+
+                     '策略收益率': [str(r * 100) + '%'],
+                     '回撤': [str(d * 100) + '%'],
+                     'sharp': [str(sharp)],
+                     '上涨率': [str(win)],
+                     'alpha': [alpha],
+                     'beta': [beta]}
+            index = pd.DataFrame(index)
+        indexes = indexes.append(index)
+        return indexes
+
+'''
+
+    def get_indexes(self, df, strategies_name=[]):
+        indexes = pd.DataFrame()
+
+        for name in strategies_name:
+            df = deepcopy(self.strategies_data[name])
+            df[self.benchmark_code + '_money'] = self.benchmark_value
+            # 年化收益率
+            r = (df.iloc[-1][name] * 1.0 / df.iloc[0][name]) ** (250 * 1.0 / df[name].__len__()) - 1
+
+            # 回撤
+            d = 0
+            t = df.iloc[0][name]
+            for i in range(1, df[name].__len__()):
+                t = max(t, df.iloc[i][name] * 1.0)
+                d = max(d, 1 - df.iloc[i][name] * 1.0 / t)
+            # sharp
+            rt = []
+            for i in range(df.index[0].year, df.index[-1].year):
+                rt.append((df[str(i)].ix[-1, name] - df[str(i)].ix[0, name]) * 1.0 / df[str(i)].ix[0, name] - 1)
+            rt = np.array(rt)
+            rt = rt * 100
+            sigma = rt.std()
+            sharp = (r - 0.028) * 100 / sigma
+
+            # 胜率
+            win = df[(df[name] - df[name].shift(-1)) > 0][name].count() * 1.0 / df.__len__()
+
+            # beta alpha
+            df[name + '_rtn'] = (df[name] - df[name].shift(-1)) / df[name] * 100
+
+            df[self.benchmark_code + '_rtn'] = (df[self.benchmark_code + '_money'] - df[
+                self.benchmark_code + '_money'].shift(-1)) / \
+                                               df[self.benchmark_code + '_money'] * 100
+            df[name + '_rtn'] = df[name + '_rtn'].map(float)
+            df[self.benchmark_code + '_rtn'] = df[self.benchmark_code + '_rtn'].map(float)
+            beta = df[name + '_rtn'][:-1].cov(df[self.benchmark_code + '_rtn'][:-1]) / df[self.benchmark_code + '_rtn'][
+                                                                                       :-1].var()
+            alpha = (r - 0.0284) - beta * (
+                    ((df.iloc[-1][self.benchmark_code + '_money'] * 1.0 / df.iloc[0][
+                        self.benchmark_code + '_money']) ** (
+                             250 * 1.0 / df.__len__()) - 1) - 0.0284)
+
+            index = {'策略收益': [str(((df.iloc[-1][name] * 1.0 / df.iloc[0][name])) * 100)],
+                     '基准收益': [str(((df.iloc[-1][self.benchmark_code + '_money'] * 1.0 / df.iloc[0][
+                         self.benchmark_code + '_money'])) * 100)],
+
+                     '策略收益率': [str(r * 100) + '%'],
+                     '回撤': [str(d * 100) + '%'],
+                     'sharp': [str(sharp)],
+                     '上涨率': [str(win)],
+                     'alpha': [alpha],
+                     'beta': [beta]}
+            index = pd.DataFrame(index)
+        indexes = indexes.append(index)
+        return indexes
+'''
--- trade_funcs.py
+++ trade_funcs.py
@@ -21,9 +21,11 @@ def trade_func(df_to_today, today, value, cash, portfolio):
     回测关键函数，可修改，定义每天回测的逻辑
 
     :param df_to_today: 从起始日期至今的收盘数据
-    :param today: （回测中）当前运行到的日期
+    :param today: date格式,（回测中）当前运行到的日期
     :param value: 资产总值
     :param cash: 当前现金
     :param portfolio: 资产配置，字典，格式为{标的物：购买金额}
     :return: 返回今天的 value, cash, portfolio
     """
+
+    return value,cash,portfolio
